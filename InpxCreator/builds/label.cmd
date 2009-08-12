b04a08f02321bf30cd5b1199f46629188a07ec9a
rupor <rupor@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-08-12 11:48:25 +0000

    Small fix

--- InpxCreator/builds/label.cmd
+++ InpxCreator/builds/label.cmd
@@ -2,7 +2,7 @@
 
 IF .%1. == .. GOTO usage
 
-svn copy http://subversion.assembla.com/svn/myhomelib/InpxCreator/trunk http://subversion.assembla.com/svn/myhomelib/InpxCreator/builds/%1
+svn copy http://subversion.assembla.com/svn/myhomelib/InpxCreator/trunk http://subversion.assembla.com/svn/myhomelib/InpxCreator/builds/%1 -m "Release %1"
 
 goto FIN
 
