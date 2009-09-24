ea353c2472030aec16012841db5671e5766d62ac
koreec <koreec@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-09-24 05:59:39 +0000

    Релиз 1.5.2

---
+++ tags/1.5.2/ImportImpl/unit_ImportInpxThread.pas
@@ -0,0 +1,414 @@
+{******************************************************************************}
+{                                                                              }
+{ MyHomeLib                                                                    }
+{                                                                              }
+{ Version 0.9                                                                  }
+{ 20.08.2008                                                                   }
+{ Copyright (c) Aleksey Penkov  alex.penkov@gmail.com                          }
+{                                                                              }
+{ @author Nick Rymanov nrymanov@gmail.com                                      }
+{                                                                              }
+{******************************************************************************}
+
+unit unit_ImportInpxThread;
+
+interface
+
+uses
+  Classes,
+  SysUtils,
+  StrUtils,
+  unit_WorkerThread,
+  unit_MHL_xml,
+  unit_globals;
+
+
+
+type
+
+  TFields = (flNone,flAuthor, flTitle, flSeries, flSerNo, flGenre, flLibID,
+             flInsideNo, flFile, flFolder, flExt, flSize, flLang, flDate,
+             flCode, flDeleted, flRate, flURI, flLibRate, flKeyWords);
+
+  TFieldDescr = record
+                   Code: string;
+                  FType: TFields;
+  end;
+
+  TImportLibRusEcThread = class(TWorker)
+  private
+
+    FDBFileName: string;
+    FCollectionRoot: string;
+    FCollectionType: Integer;
+    FInpxFileName: string;
+
+    FFields: array of TFields;
+
+    FPersonalFolder: boolean;
+
+    FVersion: integer;
+    FGenresType: TGenresType;
+
+    procedure SetCollectionRoot(const Value: string);
+    procedure  ParseData(input: WideString; var R: TBookRecord);
+
+  protected
+    procedure WorkFunction; override;
+    procedure GetFields;
+  public
+    function Import(CheckFiles: boolean):integer;
+
+    property DBFileName: string read FDBFileName write FDBFileName;
+    property CollectionRoot: string read FCollectionRoot write SetCollectionRoot;
+    property CollectionType: COLLECTION_TYPE read FCollectionType write FCollectionType;
+    property InpxFileName: string read FInpxFileName write FInpxFileName;
+    property GenresType: TGenresType write FGenresType;
+
+  end;
+
+implementation
+
+uses
+  unit_database,
+  unit_Settings,
+  unit_Consts,
+  unit_Helpers,
+  ZipForge;
+
+type
+  INPXType = (inpUnknown, inpFormat_10, inpFormat_11);
+
+const
+
+  FieldsDescr: array [1..20] of  TFieldDescr= (
+    (Code:'AUTHOR'; FType: flAuthor),
+    (Code:'TITLE';  FType:  flTitle),
+    (Code:'SERIES'; FType:  flSeries),
+    (Code:'SERNO';  FType:  flSerNo),
+    (Code:'GENRE';  FType:  flGenre),
+    (Code:'LIBID';  FType:   flLibID),
+    (Code:'INSNO';  FType:  flInsideNo),
+    (Code:'FILE';   FType:  flFile),
+    (Code:'FOLDER'; FType: flFolder),
+    (Code:'EXT';    FType:   flExt),
+    (Code:'SIZE';   FType:   flSize),
+    (Code:'LANG';   FType:  flLang),
+    (Code:'DATE';   FType:  flDate),
+    (Code:'CODE';   FType:  flCode),
+    (Code:'DEL';    FType:  flDeleted),
+    (Code:'RATE';   FType: flRate),
+    (Code:'URI';    FType:  flURI),
+    (Code:'LIBRATE';FType:  flLibRate),
+    (Code:'KEYWORDS';FType:  flKeyWords),
+    (Code:'URL';    FType:  flURI)
+  );
+
+{ TImportLibRusEcThread }
+
+function ParseString(const InputStr: String; const DelimiterChar: Char;
+  var slParams: TStringList): Boolean;
+//const
+//  DelimiterChar = Chr(4);
+var
+  nPos : integer;
+  cParam: String;
+  nParamsCount : Integer;
+begin
+  nParamsCount := 0;
+  slParams.Clear;
+  //
+  // Инициализируем выходной стринглист slParams
+  // В цикле обрабатываем строку, если символ равен разделителю - добавляем параметр в стринглист,
+  // иначе - прибавляем символ ко временной переменной
+  //
+  cParam := '';
+  for nPos := 1 to Length(InputStr) do begin
+    if InputStr[nPos] <> DelimiterChar  then
+      cParam := cParam + InputStr[nPos]
+    else begin
+      slParams.Add(cParam);
+      cParam := '';
+      inc(nParamsCount);
+    end;
+  end;
+  //
+  // Если временная переменная не пуста, добавляем её в стринглист.
+  //
+  if cParam <> '' then begin
+    slParams.Add(cParam);
+    Inc(nParamsCount);
+    cParam := '';
+  end;
+  //
+  // Проверяем соответствие количества параметров в стринглисте счётчику
+  // и выводим полученное значение как результат функции разбора
+  //
+  Result := slParams.Count = nParamsCount;
+end;
+
+
+procedure TImportLibRusEcThread.ParseData(input: WideString; var R: TBookRecord);
+const
+  DelimiterChar = Chr(4);
+var
+  p,i: integer;
+  slParams: TStringList;
+  nParamsCount : Integer;
+  AuthorList: string;
+  strLastName: string;
+  strFirstName: string;
+  strMidName: string;
+  GenreList: string;
+  s: string;
+  mm, dd, yy: word;
+
+  Max : integer;
+
+begin
+
+  R.Clear;
+  slParams := TStringList.Create;
+  try
+    ParseString(input, DelimiterChar, slParams);
+
+    // -- костыль
+    if slParams.Count < High(FFields) then
+      Max := slParams.Count - 1
+    else
+      Max := High(FFields);
+    //--
+
+    for i := 0 to Max do
+      case FFields[i] of
+        flAuthor: begin                                       // Список авторов
+                    AuthorList := slParams[i];
+                    p := PosChr(':', AuthorList);
+                    while p <> 0 do
+                    begin
+                      s := Copy(AuthorList, 1, p - 1);
+                      Delete(AuthorList, 1, p);
+
+                      p := PosChr(',', s);
+                      strLastName := Copy(s, 1, p - 1);
+                      Delete(S, 1, p);
+
+                      p := PosChr(',', s);
+                      strFirstName := Copy(s, 1, p - 1);
+                      Delete(S, 1, p);
+
+                      strMidName := S;
+
+                      R.AddAuthor(strLastName, strFirstName, strMidName);
+
+                      p := PosChr(':', AuthorList);
+                    end;
+                  end;
+        flGenre: begin                                         // Список жанров
+                   GenreList := slParams[i];
+                   p := PosChr(':', GenreList);
+                   while p <> 0 do
+                   begin
+                     if FGenresType = gtFb2 then
+                       R.AddGenreFB2('', Copy(GenreList, 1, p - 1), '')
+                     else
+                       R.AddGenreAny(Copy(GenreList, 1, p - 1), '');
+
+                     Delete(GenreList, 1, p);
+                     p := PosChr(':', GenreList);
+                    end;
+                 end;
+        flTitle:  R.Title := slParams[i];                        // Название
+        flSeries: R.Series := slParams[i];                       // Серия
+        flSerNo:  R.SeqNumber := StrToIntDef(slParams[i], 0);    // Номер внутри серии
+        flFile:   R.FileName := CheckSymbols(Trim(slParams[i])); // Имя файла
+        flExt:    R.FileExt := '.' + slParams[i];                // Тип
+        flSize:   R.Size := StrToIntDef(slParams[i], 0);         // Размер
+        flLibID:  R.LibID := StrToIntDef(slParams[i], 0);        // внутр. номер
+        flDeleted:R.Deleted := (slParams[i] = '1');              // удалена
+        flDate:   begin                                          // дата
+                    if slParams[i] <> '' then
+                    begin
+                      yy := StrToInt(Copy(slParams[i], 1, 4));
+                      mm := StrToInt(Copy(slParams[i], 6, 2));
+                      dd := StrToInt(Copy(slParams[i], 9, 2));
+                      R.Date := EncodeDate(yy, mm, dd);
+                    end
+                    else R.Date := EncodeDate(70,01,01);
+                  end;
+
+        flInsideNo:  R.InsideNo := StrToInt(slParams[i]);        // номер в архиве
+        flFolder:    R.Folder := slParams[i];                    // папка
+        flLibrate:   R.LibRate := StrToIntDef(slParams[i], 0);   // внешний рейтинг
+        flLang:      R.Lang := slParams[i];                      // язык
+        flKeyWords:  R.KeyWords := slParams[i];                  // ключевые слова
+        flURI:       R.URI := slParams[i];                  // ключевые слова
+    end; // case, for
+    R.Normalize;
+  finally
+    slParams.Free;
+  end;
+end;
+
+procedure TImportLibRusEcThread.GetFields;
+const
+  del = ';';
+  Default = 'AUTHOR;GENRE;TITLE;SERIES;SERNO;FILE;SIZE;LIBID;DEL;EXT;DATE;LANG;LIBRATE;KEYWORDS';
+  FN = 'structure.info';
+
+var
+  S: string;
+  p, i: integer;
+  F: text;
+
+  function FindType(S:string): TFields;
+  var
+    F: TFieldDescr;
+  begin
+    for F in FieldsDescr do
+      if F.Code = S then
+      begin
+        Result := F.FType;
+        Break;
+      end;
+  end;
+
+
+begin
+
+  S := '';
+  if FileExists(Settings.TempPath + FN) then
+  begin
+    AssignFile(F,Settings.TempPath + FN);
+    Reset(F);
+    Read(F,S);
+    CloseFile(F);
+
+    DeleteFile(Settings.TempPath + FN);
+  end;
+
+  if S = '' then S := Default;
+
+  // код
+  SetLength(FFields,0);
+  i :=0;
+  p := pos(del,S);
+  while p <> 0 do
+  begin
+    SetLength(FFields,i + 1);
+    FFields[i] := FindType(copy(S,1,p - 1));
+    Delete(S,1,p);
+    p := pos(del,S);
+
+    if not FPersonalFolder then
+        FPersonalFolder := (FFields[i]=flFolder);
+
+    inc(i);
+  end;
+end;
+
+function TImportLibRusEcThread.Import(CheckFiles: boolean):integer;
+var
+  FLibrary: TMHLLibrary;
+  BookList: TStringList;
+  i: Integer;
+  j: Integer;
+  R: TBookRecord;
+  filesProcessed: Integer;
+  unZip:TZipForge;
+  CurrentFile: string;
+  ArchItem: TZFArchiveItem;
+
+  FileStream : TMemoryStream;
+
+begin
+  filesProcessed := 0;
+  i := 0;
+  SetProgress(0);
+  FLibrary := TMHLLibrary.Create(nil);
+  try
+    FLibrary.DatabaseFileName := DBFileName;
+
+    FLibrary.Active := True;
+
+    FLibrary.BeginBulkOperation;
+    try
+      unZip := TZipForge.Create(Nil);
+      unZip.BaseDir := Settings.TempPath;
+      unZip.FileName := FInpxFileName;
+      unZip.OpenArchive;
+      unZip.ExtractFiles('*.*');
+      GetFields;
+      try
+        BookList := TStringListEx.Create;
+        if (unZip.FindFirst('*.inp',ArchItem,faAnyFile-faDirectory)) then
+        repeat
+          CurrentFile:= ArchItem.FileName;
+          if not(isOnlineCollection(CollectionType)) and
+            ( CurrentFile = 'extra.inp')
+            then Continue;
+
+          Teletype(Format('Обрабатываем файл %s', [CurrentFile]), tsInfo);
+          BookList.LoadFromFile(Settings.TempPath + CurrentFile,TEncoding.UTF8);
+          for j := 0 to BookList.Count - 1 do
+          begin
+            try
+              ParseData(BookList[j], R);
+              if isOnlineCollection(CollectionType) then
+              begin
+                // И\Иванов Иван\1234 Просто книга.fb2.zip
+                R.Folder := R.GenerateLocation + FB2ZIP_EXTENSION;
+                // Сохраним отметку о существовании файла
+                R.Local := FileExists(FCollectionRoot + R.Folder);
+              end
+              else
+              begin
+                if not FPersonalFolder then
+                begin
+                  // 98058-98693.inp -> 98058-98693.zip
+                  R.Folder := ChangeFileExt(CurrentFile, ZIP_EXTENSION);
+                  //
+                  R.InsideNo := j;
+                end
+              end;
+              if FLibrary.InsertBook(R, CheckFiles, False)<>0 then
+                    Inc(filesProcessed);
+              if (filesProcessed mod ProcessedItemThreshold) = 0 then
+              begin
+                SetProgress(round((i + j/BookList.Count) * 100 / unZip.FileCount));
+                SetComment(Format('Добавлено %u книг', [filesProcessed]));
+              end;
+            except
+              on EConvertError do
+                    Teletype(Format('Ошибка структуры inp. Файл %s  Строка %u ', [CurrentFile,j]), tsError);
+            end;
+          end;
+          inc(i);
+          if Canceled then
+            Break;
+        until (not unZip.FindNext(ArchItem));
+      finally
+        BookList.Free;
+      end;
+      Teletype(Format('Добавлено %u книг', [filesProcessed]), tsInfo);
+    finally
+      unZip.Free;
+    end;
+  finally
+    FLibrary.EndBulkOperation;
+    FLibrary.Free;
+  end;
+end;
+
+procedure TImportLibRusEcThread.SetCollectionRoot(const Value: string);
+begin
+  FCollectionRoot := IncludeTrailingPathDelimiter(Value);
+end;
+
+procedure TImportLibRusEcThread.WorkFunction;
+begin
+  Import(False);
+end;
+
+end.
+
---
+++ tags/1.5.2/MyhomeLib.dpr
@@ -0,0 +1,142 @@
+
+{******************************************************************************}
+{                                                                              }
+{                                 MyHomeLib                                    }
+{                                                                              }
+{              Copyright (c) 2008, Aleksey Penkov  alex.penkov@gmail.com       }
+{                                                                              }
+{                               http://home-lib.net                            }
+{                                                                              }
+{ This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.                               }
+{                                                                              }
+{******************************************************************************}
+
+program MyhomeLib;
+
+{$R *.dres}
+
+uses
+  Forms,
+  IMouse,
+  frm_main in 'Forms\frm_main.pas' {frmMain},
+  dm_user in 'DataModules\dm_user.pas' {DMUser: TDataModule},
+  unit_Database in 'Units\unit_Database.pas',
+  unit_Globals in 'Units\unit_Globals.pas',
+  frm_settings in 'Forms\frm_settings.pas' {frmSettings},
+  frm_about in 'Forms\frm_about.pas' {frmAbout},
+  frm_statistic in 'Forms\frm_statistic.pas' {frmStat},
+  unit_PrevInst in 'Units\unit_PrevInst.pas',
+  frm_bases in 'Forms\frm_bases.pas' {frmBases},
+  frm_book_info in 'Forms\frm_book_info.pas' {frmBookDetails},
+  frm_splash in 'Forms\frm_splash.pas' {frmSplash},
+  frm_add_nonfb2 in 'Forms\frm_add_nonfb2.pas' {frmAddnonfb2},
+  frm_edit_book_info in 'Forms\frm_edit_book_info.pas' {frmEditBookInfo},
+  frm_genre_tree in 'Forms\frm_genre_tree.pas' {frmGenreTree},
+  unit_TreeUtils in 'Units\unit_TreeUtils.pas',
+  unit_MHL_xml in 'Units\unit_MHL_xml.pas',
+  frm_edit_author in 'Forms\frm_edit_author.pas' {frmEditAuthor},
+  frm_edit_reader in 'Forms\frm_edit_reader.pas' {frmEditReader},
+  unit_MHL_strings in 'Units\unit_MHL_strings.pas',
+  frm_edit_script in 'Forms\frm_edit_script.pas' {frmEditScript},
+  unit_Settings in 'Units\unit_Settings.pas',
+  unit_Readers in 'Units\unit_Readers.pas',
+  unit_Scripts in 'Units\unit_Scripts.pas',
+  unit_Errors in 'Units\unit_Errors.pas',
+  unit_Consts in 'Units\unit_Consts.pas',
+  unit_WorkerThread in 'ImportImpl\unit_WorkerThread.pas',
+  unit_ImportXMLThread in 'ImportImpl\unit_ImportXMLThread.pas',
+  unit_Import in 'ImportImpl\unit_Import.pas',
+  frm_BaseProgressForm in 'ImportImpl\frm_BaseProgressForm.pas' {ProgressFormBase},
+  frm_ImportProgressForm in 'ImportImpl\frm_ImportProgressForm.pas' {ImportProgressForm},
+  unit_ImportFB2Thread in 'ImportImpl\unit_ImportFB2Thread.pas',
+  unit_Helpers in 'Units\unit_Helpers.pas',
+  frm_ImportProgressFormEx in 'ImportImpl\frm_ImportProgressFormEx.pas' {ImportProgressFormEx},
+  unit_ImportFB2ThreadBase in 'ImportImpl\unit_ImportFB2ThreadBase.pas',
+  unit_ImportFB2ZIPThread in 'ImportImpl\unit_ImportFB2ZIPThread.pas',
+  unit_ImportInpxThread in 'ImportImpl\unit_ImportInpxThread.pas',
+  unit_ExportXMLThread in 'ImportImpl\unit_ExportXMLThread.pas',
+  unit_Export in 'ImportImpl\unit_Export.pas',
+  frm_ExportProgressForm in 'ImportImpl\frm_ExportProgressForm.pas' {ExportProgressForm},
+  frm_SyncOnLineProgressForm in 'UtilsImpl\frm_SyncOnLineProgressForm.pas' {SyncOnLineProgressForm},
+  unit_SyncOnLineThread in 'UtilsImpl\unit_SyncOnLineThread.pas',
+  unit_Utils in 'UtilsImpl\unit_Utils.pas',
+  frm_ExportToDeviceProgressForm in 'UtilsImpl\frm_ExportToDeviceProgressForm.pas' {ExportToDeviceProgressForm},
+  unit_ExportToDevice in 'UtilsImpl\unit_ExportToDevice.pas',
+  unit_ExportToDeviceThread in 'UtilsImpl\unit_ExportToDeviceThread.pas',
+  frame_DecorativePageBase in 'Wizards\NewCollection\frame_DecorativePageBase.pas' {DecorativePageBase: TFrame},
+  frame_InteriorPageBase in 'Wizards\NewCollection\frame_InteriorPageBase.pas' {InteriorPageBase: TFrame},
+  frame_NCWWelcom in 'Wizards\NewCollection\frame_NCWWelcom.pas' {frameNCWWelcom: TFrame},
+  frm_NCWizard in 'Wizards\NewCollection\frm_NCWizard.pas' {frmNCWizard},
+  frame_NCWOperation in 'Wizards\NewCollection\frame_NCWOperation.pas' {frameNCWOperation: TFrame},
+  frame_NCWFinish in 'Wizards\NewCollection\frame_NCWFinish.pas' {frameNCWFinish: TFrame},
+  frame_NCWCollectionType in 'Wizards\NewCollection\frame_NCWCollectionType.pas' {frameNCWCollectionType: TFrame},
+  frame_NCWCollectionNameAndLocation in 'Wizards\NewCollection\frame_NCWCollectionNameAndLocation.pas' {frameNCWNameAndLocation: TFrame},
+  frame_NCWSelectGenreFile in 'Wizards\NewCollection\frame_NCWSelectGenreFile.pas' {frameNCWSelectGenreFile: TFrame},
+  frame_NCWProgress in 'Wizards\NewCollection\frame_NCWProgress.pas' {frameNCWProgress: TFrame},
+  frame_WizardPageBase in 'Wizards\NewCollection\frame_WizardPageBase.pas' {WizardPageBase: TFrame},
+  frame_NCWImport in 'Wizards\NewCollection\frame_NCWImport.pas' {frameNCWImport: TFrame},
+  unit_NCWParams in 'Wizards\NewCollection\unit_NCWParams.pas',
+  frame_NCWCollectionFileTypes in 'Wizards\NewCollection\frame_NCWCollectionFileTypes.pas' {frameNCWCollectionFileTypes: TFrame},
+  unit_fb2ToText in 'UtilsImpl\unit_fb2ToText.pas',
+  unit_SyncFoldersThread in 'UtilsImpl\unit_SyncFoldersThread.pas',
+  unit_libupdateThread in 'UtilsImpl\unit_libupdateThread.pas',
+  unit_DownloadBooksThread in 'DwnldImpl\unit_DownloadBooksThread.pas',
+  frm_DownloadProgressForm in 'DwnldImpl\frm_DownloadProgressForm.pas' {DownloadProgressForm},
+  unit_Columns in 'Units\unit_Columns.pas',
+  StoHtmlHelp in 'UtilsImpl\StoHtmlHelp.pas',
+  unit_DownloadManagerThread in 'DwnldImpl\unit_DownloadManagerThread.pas',
+  unit_Messages in 'Units\unit_Messages.pas',
+  unit_Lib_Updates in 'Units\unit_Lib_Updates.pas',
+  frm_editor in 'Forms\frm_editor.pas' {frmEditor},
+  unit_SearchUtils in 'Units\unit_SearchUtils.pas',
+  unit_ExportINPXThread in 'ImportImpl\unit_ExportINPXThread.pas',
+  frm_info_popup in 'Forms\frm_info_popup.pas' {frmInfoPopup},
+  dm_collection in 'DataModules\dm_collection.pas' {DMCollection: TDataModule},
+  htmlhlp in 'Units\htmlhlp.pas',
+  frm_search in 'Forms\frm_search.pas' {frmBookSearch},
+  unit_ReviewParser in 'Units\unit_ReviewParser.pas',
+  unit_WriteFb2Info in 'Units\unit_WriteFb2Info.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+
+  if FirstHinstanceRunning(1) then
+  begin
+    Exit;
+  end;
+
+  Application.MainFormOnTaskbar := True;
+  Application.Title := 'MyHomeLib';
+  Application.HelpFile := '';
+  frmSplash := TfrmSplash.Create(Application);
+  try
+    frmSplash.Show;   // Display the splash screen
+    frmSplash.Update; // Update the splash screen to ensure it gets drawn
+
+  // ¬ажно! сначала создаем датамодули
+  Application.CreateForm(TDMUser, DMUser);
+  Application.CreateForm(TDMCollection, DMCollection);
+  // а потом - формы!
+  Application.CreateForm(TfrmMain, frmMain);
+  Application.CreateForm(TfrmAddnonfb2, frmAddnonfb2);
+  Application.CreateForm(TfrmEditor, frmEditor);
+  //  Application.CreateForm(TfrmAddnonfb2, frmAddnonfb2);
+  Application.CreateForm(TfrmEditBookInfo, frmEditBookInfo);
+  Application.CreateForm(TfrmGenreTree, frmGenreTree);
+  Application.CreateForm(TfrmEditAuthor, frmEditAuthor);
+  frmSplash.Hide;  // Hide the splash screen
+  finally
+    frmSplash.Free;  // Free the splash screen
+  end;
+  Application.Run;
+end.
