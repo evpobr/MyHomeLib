28856601ae89bda4be6e792c862222a11a1c7e61
koreec <koreec@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2010-01-14 02:27:40 +0000

    [*] экспорт пользовательских данных

--- branches/1.6.1/Forms/frm_main.pas
+++ branches/1.6.1/Forms/frm_main.pas
@@ -1107,14 +1107,6 @@ begin
   CreateSettings;
   Settings.LoadSettings;
 
-  WindowState := Settings.WindowState;
-
-  if WindowState = wsNormal then
-  begin
-    Width := Settings.FormWidth;
-    Height := Settings.FormHeight;
-  end;
-
   SetColors;
 
   tlbrEdit.Visible := Settings.EditToolbarVisible;
@@ -2229,12 +2221,6 @@ begin
   FStarImage := CreateImageFromResource(TPngImage, 'smallStar') as TPngImage;
   FEmptyStarImage := CreateImageFromResource(TPngImage, 'smallStarEmpty') as TPngImage;
 
-  // костыль
-  frmMain.Visible := True;
-  if frmMain.WindowState = wsMinimized then
-     frmMain.WindowState := wsNormal;
-  // конец костыля
-
   // загрузка списка пресетов для поиска
   CreateDir(Settings.PresetDir);
   FilesList.TargetPath := Settings.PresetPath;
@@ -2252,6 +2238,21 @@ begin
 
   if Settings.AutoStartDwnld then
     btnStartDownloadClick(Sender);
+
+
+  WindowState := Settings.WindowState;
+
+  if WindowState = wsNormal then
+  begin
+    Width := Settings.FormWidth;
+    Height := Settings.FormHeight;
+  end;
+
+  // костыль
+  frmMain.Visible := True;
+  if frmMain.WindowState = wsMinimized then
+     frmMain.WindowState := wsNormal;
+  // конец костыля
 
 end;
 
@@ -3506,8 +3507,13 @@ begin
       begin
         dmCollection.GetBookFileName(Data.ID, FileName, Folder, Ext, No);
 
+     if ActiveView <> FavoritesView then
         WorkFile := Settings.ReadPath + Format('%s - %s.%d%s',
-                                              [CheckSymbols(Panel.Author),
+                                              [CheckSymbols(dmCollection.FullName(Data.ID)),
+                                               CheckSymbols(Panel.Title),Id,Ext])
+      else
+         WorkFile := Settings.ReadPath + Format('%s - %s.%d%s',
+                                              [CheckSymbols(DMUser.tblGrouppedBooksFullName.Value),
                                                CheckSymbols(Panel.Title),Id,Ext]);
 
         if not FileExists(WorkFile) then
@@ -6068,7 +6074,13 @@ begin
     DMUser.tblRates.First;
     while not DMUser.tblRates.Eof do
     begin
-      SL.Add(Format('%d %d',[DMUser.tblRatesBookID.Value, DMUser.tblRatesRate.Value]));
+      DMCollection.tblBooks.Locate('ID', DMUser.tblRatesBookID.Value, []);
+      if DMCollection.tblBooksLibID.Value <> 0 then
+        ID := DMCollection.tblBooksLibID.Value
+      else
+        ID := DMCollection.tblBooksID.Value;
+
+      SL.Add(Format('%d %d',[ID, DMUser.tblRatesRate.Value]));
       DMUser.tblRates.Next;
     end;
     DMUser.tblRates.Filtered := False;
@@ -6081,7 +6093,13 @@ begin
     DMUser.tblFinished.First;
     while not DMUser.tblFinished.Eof do
     begin
-      SL.Add(Format('%d %d',[DMUser.tblFinishedBookID.Value, DMUser.tblFinishedProgress.Value]));
+      DMCollection.tblBooks.Locate('ID', DMUser.tblFinishedBookID.Value, []);
+      if DMCollection.tblBooksLibID.Value <> 0 then
+        ID := DMCollection.tblBooksLibID.Value
+      else
+        ID := DMCollection.tblBooksID.Value;
+
+      SL.Add(Format('%d %d',[ID, DMUser.tblFinishedProgress.Value]));
       DMUser.tblFinished.Next;
     end;
     DMUser.tblFinished.Filtered := False;
@@ -6121,7 +6139,13 @@ begin
     begin
       S := DMCollection.tblExtraReview.Value;
       StrReplace(#13#10,'~',S);
-      SL.Add(Format('%d %s',[DMCollection.tblExtraID.Value, S]));
+      DMCollection.tblBooks.Locate('ID', DMCollection.tblExtraBookID.Value, []);
+      if DMCollection.tblBooksLibID.Value <> 0 then
+        ID := DMCollection.tblBooksLibID.Value
+      else
+        ID := DMCollection.tblBooksID.Value;
+
+      SL.Add(Format('%d %s',[ID, S]));
       DMCollection.tblExtra.Next;
     end;
     DMCollection.tblExtra.MasterSource := DMCollection.dsBooks;
--- branches/1.6.1/Forms/frm_splash.dfm
+++ branches/1.6.1/Forms/frm_splash.dfm
@@ -622,7 +622,7 @@ object frmSplash: TfrmSplash
     Width = 48
     Height = 23
     Alignment = taRightJustify
-    Caption = '1.6.1'
+    Caption = '1.6.2'
     Font.Charset = DEFAULT_CHARSET
     Font.Color = 259580
     Font.Height = -19
