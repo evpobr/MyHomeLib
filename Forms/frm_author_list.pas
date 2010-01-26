7e64174bf76c66517886fe107df6b3dd7de8d736
nrymanov <nrymanov@gmail.com>
2010-01-26 15:03:20 +0000

    * постепенное привидение к стандартному внешнему виду

--- Forms/frm_author_list.pas
+++ Forms/frm_author_list.pas
@@ -1,25 +1,34 @@
+(* *****************************************************************************
+  *
+  * MyHomeLib
+  *
+  * Version 0.9
+  * 20.08.2008
+  * Copyright (c) Aleksey Penkov  alex.penkov@gmail.com
+  *               Nick Rymanov    nrymanov@gmail.com
+  ****************************************************************************** *)
+
 unit frm_author_list;
 
 interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, VirtualTrees, RzButton, ExtCtrls, RzPanel;
+  Dialogs, VirtualTrees, ExtCtrls, StdCtrls;
 
 type
   TfrmAuthorList = class(TForm)
-    RzPanel2: TRzPanel;
-    RzBitBtn1: TRzBitBtn;
-    RzButton1: TRzButton;
     tvAuthorList: TVirtualStringTree;
-    procedure tvAuthorListGetText(Sender: TBaseVirtualTree; Node: PVirtualNode;
-      Column: TColumnIndex; TextType: TVSTTextType; var CellText: string);
-    procedure tvAuthorListGetNodeDataSize(Sender: TBaseVirtualTree;
-      var NodeDataSize: Integer);
+    pnButtons: TPanel;
+    btnOk: TButton;
+    btnCancel: TButton;
+    procedure tvAuthorListGetText(Sender: TBaseVirtualTree; Node: PVirtualNode; Column: TColumnIndex;
+      TextType: TVSTTextType; var CellText: string);
+    procedure tvAuthorListGetNodeDataSize(Sender: TBaseVirtualTree; var NodeDataSize: Integer);
   private
-    { Private declarations }
+
   public
-    { Public declarations }
+
   end;
 
 var
@@ -30,19 +39,17 @@ implementation
 uses
   unit_Globals;
 
-{$R *.dfm}
+  {$R *.dfm}
 
-procedure TfrmAuthorList.tvAuthorListGetNodeDataSize(Sender: TBaseVirtualTree;
-  var NodeDataSize: Integer);
+procedure TfrmAuthorList.tvAuthorListGetNodeDataSize(Sender: TBaseVirtualTree; var NodeDataSize: Integer);
 begin
   NodeDataSize := Sizeof(TAuthorData);
 end;
 
-procedure TfrmAuthorList.tvAuthorListGetText(Sender: TBaseVirtualTree;
-  Node: PVirtualNode; Column: TColumnIndex; TextType: TVSTTextType;
-  var CellText: string);
+procedure TfrmAuthorList.tvAuthorListGetText(Sender: TBaseVirtualTree; Node: PVirtualNode; Column: TColumnIndex;
+  TextType: TVSTTextType; var CellText: string);
 var
-  Data:PAuthorData;
+  Data: PAuthorData;
 begin
   Data := tvAuthorList.GetNodeData(Node);
   CellText := Data.Text;
