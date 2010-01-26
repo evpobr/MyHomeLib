76fe5591bd05a799a8ecea5c9adc6bc9114c012b
nrymanov <nrymanov@gmail.com>
2010-01-26 14:58:52 +0000

    * постепенное привидение к стандартному внешнему виду

--- Forms/frm_genre_tree.pas
+++ Forms/frm_genre_tree.pas
@@ -1,14 +1,12 @@
-
-{******************************************************************************}
-{                                                                              }
-{                                 MyHomeLib                                    }
-{                                                                              }
-{                                Version 0.9                                   }
-{                                20.08.2008                                    }
-{                    Copyright (c) Aleksey Penkov  alex.penkov@gmail.com       }
-{                                                                              }
-{******************************************************************************}
-
+(* *****************************************************************************
+  *
+  * MyHomeLib
+  *
+  * Version 0.9
+  * 20.08.2008
+  * Copyright (c) Aleksey Penkov  alex.penkov@gmail.com
+  *
+  ****************************************************************************** *)
 
 unit frm_genre_tree;
 
@@ -16,27 +14,22 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, VirtualTrees, RzButton, ExtCtrls, RzPanel, unit_globals;
+  Dialogs, VirtualTrees, RzButton, ExtCtrls, RzPanel, unit_globals, StdCtrls;
 
 type
   TfrmGenreTree = class(TForm)
-    RzPanel1: TRzPanel;
-    RzPanel2: TRzPanel;
     tvGenresTree: TVirtualStringTree;
-    RzBitBtn1: TRzBitBtn;
-    RzButton1: TRzButton;
-    procedure tvGenresTreeGetNodeDataSize(Sender: TBaseVirtualTree;
-      var NodeDataSize: Integer);
-    procedure tvGenresTreeGetText(Sender: TBaseVirtualTree; Node: PVirtualNode;
-      Column: TColumnIndex; TextType: TVSTTextType; var CellText: String);
+    pnButtons: TPanel;
+    btnOk: TButton;
+    btnCancel: TButton;
+    procedure tvGenresTreeGetNodeDataSize(Sender: TBaseVirtualTree; var NodeDataSize: Integer);
+    procedure tvGenresTreeGetText(Sender: TBaseVirtualTree; Node: PVirtualNode; Column: TColumnIndex;
+      TextType: TVSTTextType; var CellText: String);
   private
 
-
-    { Private declarations }
   public
-    { Public declarations }
     procedure GetSelectedGenres(var R: TBookRecord);
-    procedure SelectGenres(Code:String);
+    procedure SelectGenres(Code: String);
   end;
 
 var
@@ -44,64 +37,60 @@ var
 
 implementation
 
-uses unit_Consts;
+uses
+  unit_Consts;
 
 {$R *.dfm}
 
-procedure TfrmGenreTree.SelectGenres(Code:String);
+procedure TfrmGenreTree.SelectGenres(Code: String);
 var
-  Node:PVirtualNode;
-  Data:PGenreData;
+  Node: PVirtualNode;
+  Data: PGenreData;
 begin
-  Node:=tvGenresTree.GetFirst;
-  while Node<>nil do
+  Node := tvGenresTree.GetFirst;
+  while Node <> nil do
   begin
-    Data:=tvGenresTree.GetNodeData(Node);
-    if Data.Code=Code then
+    Data := tvGenresTree.GetNodeData(Node);
+    if Data.Code = Code then
     begin
-      tvGenresTree.Selected[Node]:=True;
-      tvGenresTree.Expanded[Node.Parent]:=True;
+      tvGenresTree.Selected[Node] := True;
+      tvGenresTree.Expanded[Node.Parent] := True;
     end;
-    Node:=tvGenresTree.GetNext(Node);
-  end;  
+    Node := tvGenresTree.GetNext(Node);
+  end;
 end;
 
-
-
-procedure TfrmGenreTree.GetSelectedGenres(var R:TBookRecord);
+procedure TfrmGenreTree.GetSelectedGenres(var R: TBookRecord);
 var
-  Data:PGenreData;
-  Node:PvirtualNode;
+  Data: PGenreData;
+  Node: PVirtualNode;
 begin
-  Node:=tvGenresTree.GetFirstSelected;
+  Node := tvGenresTree.GetFirstSelected;
 
   R.ClearGenres;
   while Assigned(Node) do
   begin
-    Data:=frmGenreTree.tvGenresTree.GetNodeData(Node);
+    Data := frmGenreTree.tvGenresTree.GetNodeData(Node);
     R.AddGenreFB2(Data.Code, Data.FB2Code, '');
-    Node:=tvGenresTree.GetNextSelected(Node);
+    Node := tvGenresTree.GetNextSelected(Node);
   end;
 
   if R.GenreCount = 0 then
     R.AddGenreFB2(UNKNOWN_GENRE_CODE, '', '');
 end;
 
-procedure TfrmGenreTree.tvGenresTreeGetNodeDataSize(Sender: TBaseVirtualTree;
-  var NodeDataSize: Integer);
+procedure TfrmGenreTree.tvGenresTreeGetNodeDataSize(Sender: TBaseVirtualTree; var NodeDataSize: Integer);
 begin
-  NodeDataSize:=Sizeof(TGenreData);
+  NodeDataSize := Sizeof(TGenreData);
 end;
 
-
-procedure TfrmGenreTree.tvGenresTreeGetText(Sender: TBaseVirtualTree;
-  Node: PVirtualNode; Column: TColumnIndex; TextType: TVSTTextType;
-  var CellText: String);
+procedure TfrmGenreTree.tvGenresTreeGetText(Sender: TBaseVirtualTree; Node: PVirtualNode; Column: TColumnIndex;
+  TextType: TVSTTextType; var CellText: String);
 var
-  Data:PGenreData;
+  Data: PGenreData;
 begin
-  Data:=tvGenresTree.GetNodeData(Node);
-  CellText:=Data.Text;
+  Data := tvGenresTree.GetNodeData(Node);
+  CellText := Data.Text;
 end;
 
 end.
