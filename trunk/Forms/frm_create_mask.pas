cec01f8fafa51e2bebb5dc4f4fea9a913fdc120a
nrymanov@gmail.com <nrymanov@gmail.com@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2010-01-29 14:38:33 +0000

    see #29 - Редизайн и унификация пользовательского интерфейса

--- trunk/Forms/frm_create_mask.pas
+++ trunk/Forms/frm_create_mask.pas
@@ -1,19 +1,31 @@
+(* *****************************************************************************
+  *
+  * MyHomeLib
+  *
+  * Version 0.9
+  * 20.08.2008
+  * Copyright (c) Aleksey Penkov  alex.penkov@gmail.com
+  *               Nick Rymanov    nrymanov@gmail.com
+  ****************************************************************************** *)
+
 unit frm_create_mask;
 
 interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, Buttons, StdCtrls, RzButton, ExtCtrls;
+  Dialogs, Buttons, StdCtrls, ExtCtrls, unit_StaticTip;
 
 type
   TfrmCreateMask = class(TForm)
     edTemplate: TEdit;
     Label1: TLabel;
     Label2: TLabel;
+    pnButtons: TPanel;
+    btnOk: TButton;
+    btnCancel: TButton;
+    stDescription: TMHLStaticTip;
     Label3: TLabel;
-    RzBitBtn1: TRzBitBtn;
-    RzBitBtn2: TRzBitBtn;
     procedure RzBitBtn1Click(Sender: TObject);
   private
     { Private declarations }
@@ -27,73 +39,77 @@ var
 implementation
 
 uses frm_settings;
-
 {$R *.dfm}
 
 procedure TfrmCreateMask.RzBitBtn1Click(Sender: TObject);
-const mask_elements: array[1..4] of string = ('f', 's', 'n', 't');
-type TElement = record
-                  name: string;
-                  BegBlock, EndBlock: integer;
-                end;
-var stack: array[1..10] of TElement;
-    Elements: array[1..10] of TElement;
-    Template: string;
-    i, j, StackPos, ElementPos: integer;
-    bol: boolean;
+const
+  mask_elements: array [1 .. 4] of string = ('f', 's', 'n', 't');
+type
+  TElement = record
+    name: string;
+    BegBlock, EndBlock: integer;
+  end;
+var
+  stack: array [1 .. 10] of TElement;
+  Elements: array [1 .. 10] of TElement;
+  Template: string;
+  i, j, StackPos, ElementPos: integer;
+  bol: boolean;
 begin
-  Template:= edTemplate.Text;
-  StackPos:= 0;
-  ElementPos:= 0;
-  i:= 1;
+  Template := edTemplate.Text;
+  StackPos := 0;
+  ElementPos := 0;
+  i := 1;
   while i <= Length(Template) do
   begin
-    if Template[i]='[' then
+    if Template[i] = '[' then
     begin
       inc(StackPos);
-      Stack[StackPos].BegBlock:= i;
-      Stack[StackPos].name:= '';
+      stack[StackPos].BegBlock := i;
+      stack[StackPos].name := '';
     end;
 
-    if Template[i]='%' then
+    if Template[i] = '%' then
     begin
-      if (Stack[StackPos].name <> '') and (StackPos > 0) then
+      if (stack[StackPos].name <> '') and (StackPos > 0) then
       begin
+        // TODO -oNickR -cRefactoring : использовать общую функцию для пока сообщения об ошибке
         ShowMessage('Проверьте правильность шаблона');
         exit;
       end;
 
       inc(i);
-      Stack[StackPos].name:= '';
-      while CharInSet(Template[i], ['a'..'z', 'A'..'Z']) do
+      stack[StackPos].name := '';
+      while CharInSet(Template[i], ['a' .. 'z', 'A' .. 'Z']) do
       begin
-        Stack[StackPos].name:= Stack[StackPos].name + Template[i];
+        stack[StackPos].name := stack[StackPos].name + Template[i];
         inc(i);
       end;
 
       if StackPos <= 0 then
       begin
         inc(ElementPos);
-        Elements[ElementPos].name:= Stack[StackPos].name;
-        Elements[ElementPos].BegBlock:= 0;
-        Elements[ElementPos].EndBlock:= 0;
+        Elements[ElementPos].name := stack[StackPos].name;
+        Elements[ElementPos].BegBlock := 0;
+        Elements[ElementPos].EndBlock := 0;
       end;
     end;
 
-    if Template[i]=']' then
+    if Template[i] = ']' then
     begin
-      if (Stack[StackPos].name = '') or (StackPos <= 0) then
+      if (stack[StackPos].name = '') or (StackPos <= 0) then
       begin
+        // TODO -oNickR -cRefactoring : использовать общую функцию для пока сообщения об ошибке
         ShowMessage('Проверьте правильность шаблона');
         exit;
       end;
 
-      Stack[StackPos].EndBlock:= i;
+      stack[StackPos].EndBlock := i;
 
       inc(ElementPos);
-      Elements[ElementPos].name:= Stack[StackPos].name;
-      Elements[ElementPos].BegBlock:= Stack[StackPos].BegBlock;
-      Elements[ElementPos].EndBlock:= Stack[StackPos].EndBlock;
+      Elements[ElementPos].name := stack[StackPos].name;
+      Elements[ElementPos].BegBlock := stack[StackPos].BegBlock;
+      Elements[ElementPos].EndBlock := stack[StackPos].EndBlock;
 
       dec(StackPos);
     end;
@@ -103,10 +119,10 @@ begin
 
   for i := 1 to ElementPos do
   begin
-    bol:= false;
+    bol := false;
     for j := 1 to High(mask_elements) do
       if Elements[i].name = mask_elements[j] then
-        bol:= true;
+        bol := true;
 
     if not(bol) then
       break;
@@ -114,11 +130,12 @@ begin
 
   if not(bol) then
   begin
+    // TODO -oNickR -cRefactoring : использовать общую функцию для пока сообщения об ошибке
     ShowMessage('Неверные элементы шаблона');
     exit;
   end;
 
-ModalResult:= mrOk;
+  ModalResult := mrOk;
 end;
 
 end.
