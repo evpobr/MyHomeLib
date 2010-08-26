4cd473d39a25e78ea8b18575f6fe46d7afdb38a3
eg_ <eg_@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2010-08-26 09:35:29 +0000

    See #73 - fix the bug caused by line feed characters in the query

--- trunk/Program/Units/unit_SearchUtils.pas
+++ trunk/Program/Units/unit_SearchUtils.pas
@@ -35,6 +35,8 @@ begin
   if Value = '' then
     Exit;
 
+  StrReplace(LF, ' ', Value);
+  StrReplace(CRLF, ' ', Value);
   StrReplace('NOT LIKE', 'NOT#LIKE', Value);
   StrReplace(' LIKE', ' ' + Field + '#LIKE', Value);
   StrReplace(' =', ' ' + Field + #7 + '=', Value);
