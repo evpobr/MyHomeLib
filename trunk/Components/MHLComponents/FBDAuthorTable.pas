5cc0420336538069531be4b66d8604a2a3e8015f
koreec <koreec@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2011-05-15 02:22:52 +0000

    * положение кнопок

--- trunk/Components/MHLComponents/FBDAuthorTable.pas
+++ trunk/Components/MHLComponents/FBDAuthorTable.pas
@@ -450,7 +450,7 @@ begin
   begin
     Parent := Self;
     Left := FList.Left;
-    Top := FList.Top + FList.Height + 15;
+    Top := FList.Top + FList.Height + 5;
     Width := 75;
     Caption := rstrAdd;
     TabOrder := 1;
