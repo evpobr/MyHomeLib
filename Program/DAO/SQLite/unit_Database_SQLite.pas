evpobr/MyHomeLib

commit 0685882b0c732579298f70bd9e3e1864725cb3db

Use native System.Sqlite unit

Program/DAO/SQLite/unit_Database_SQLite.pas
@@ -255,7 +255,7 @@ uses
   Character,
   Generics.Collections,
   Variants,
-  SQLite3,
+  System.Sqlite,
   DateUtils,
   Math,
   StrUtils,
@@ -1033,44 +1033,46 @@ end;
 //-----------------------------------------------------------------------------
 
 { TBookCollection_SQLite }
-function CreateFullAuthorName(pCtx: TSQLite3Context; nArgs: Integer; Args: TSQLite3Value): string; inline;
+function CreateFullAuthorName(Context: sqlite3_context; Num: Integer; out Value: sqlite3_value): string; inline;
 var
   LastName: string;
   FirstName: string;
   MiddleName: string;
 begin
-  LastName := SQLite3_Value_text16(Args^); Inc(Args);
-  FirstName := SQLite3_Value_text16(Args^); Inc(Args);
-  MiddleName := SQLite3_Value_text16(Args^);
+  LastName := SQLite3_Value_text16(Value);
+  Inc(NativeInt(Value));
+  FirstName := SQLite3_Value_text16(Value);
+  Inc(NativeInt(Value));
+  MiddleName := SQLite3_Value_text16(Value);
 
   Result := TAuthorData.FormatName(LastName, FirstName, MiddleName);
 end;
 
-procedure fullAuthorName(pCtx: TSQLite3Context; nArgs: Integer; Args: TSQLite3Value); cdecl;
+procedure fullAuthorName(Context: sqlite3_context; Num: Integer; out Value: sqlite3_value); cdecl;
 var
   FullName: string;
 begin
-  FullName := CreateFullAuthorName(pCtx, nArgs, Args);
-  SQLite3_Result_Text16(pCtx, PWideChar(FullName), -1, SQLITE_TRANSIENT);
+  FullName := CreateFullAuthorName(Context, Num, Value);
+  SQLite3_Result_Text16(Context, PWideChar(FullName), -1, TResultDestructor(SQLITE_TRANSIENT));
 end;
 
-procedure fullAuthorNameEx(pCtx: TSQLite3Context; nArgs: Integer; Args: TSQLite3Value); cdecl;
+procedure fullAuthorNameEx(Context: sqlite3_context; Num: Integer; out Value: sqlite3_value); cdecl;
 var
   FullName: string;
 begin
-  FullName := TCharacter.ToUpper(CreateFullAuthorName(pCtx, nArgs, Args));
-  SQLite3_Result_Text16(pCtx, PWideChar(FullName), -1, SQLITE_TRANSIENT);
+  FullName := TCharacter.ToUpper(CreateFullAuthorName(Context, Num, Value));
+  SQLite3_Result_Text16(Context, PWideChar(FullName), -1, TResultDestructor(SQLITE_TRANSIENT));
 end;
 
-procedure getIsTriggersOn(pCtx: TSQLite3Context; nArgs: Integer; Args: TSQLite3Value); cdecl;
+procedure getIsTriggersOn(Context: sqlite3_context; Num: Integer; out Value: sqlite3_value); cdecl;
 var
   db: TBookCollection_SQLite;
 begin
-  db := TBookCollection_SQLite(SQLite3_User_Data(pCtx));
+  db := TBookCollection_SQLite(SQLite3_User_Data(Context));
   if Assigned(db) then
-    SQLite3_Result_Int(pCtx, IfThen(db.FTriggersEnabled, 1, 0))
+    SQLite3_Result_Int(Context, IfThen(db.FTriggersEnabled, 1, 0))
   else
-    SQLite3_Result_Int(pCtx, 1);
+    SQLite3_Result_Int(Context, 1);
 end;
 
 constructor TBookCollection_SQLite.Create(const CollectionInfo: TCollectionInfo; const SystemData: ISystemData);

Program/DAO/SQLite/Lib/SQLite3UDF.pas
@@ -19,12 +19,12 @@ unit SQLite3UDF;
 interface
 
 uses
-  SQLite3;
+  System.Sqlite;
 
 function SystemCollate(Userdta: Pointer; Buf1Len: Integer; Buf1: Pointer; Buf2Len: Integer; Buf2: Pointer): Integer; cdecl;
 function SystemCollateNoCase(Userdta: Pointer; Buf1Len: Integer; Buf1: Pointer; Buf2Len: Integer; Buf2: Pointer): Integer; cdecl;
-procedure SystemUpperString(pCtx: TSQLite3Context; nArgs: Integer; Args: TSQLite3Value); cdecl;
-procedure SystemLowerString(pCtx: TSQLite3Context; nArgs: Integer; Args: TSQLite3Value); cdecl;
+procedure SystemUpperString(Context: sqlite3_context; Num: Integer; out Value: sqlite3_value); cdecl;
+procedure SystemLowerString(Context: sqlite3_context; Num: Integer; out Value: sqlite3_value); cdecl;
 
 implementation
 
@@ -52,20 +52,20 @@ begin
   ) - CSTR_EQUAL;
 end;
 
-procedure SystemUpperString(pCtx: TSQLite3Context; nArgs: Integer; Args: TSQLite3Value); cdecl;
+procedure SystemUpperString(Context: sqlite3_context; Num: Integer; out Value: sqlite3_value); cdecl;
 var
   s: string;
 begin
-  s := SQLite3_Value_text16(Args^);
-  SQLite3_Result_Text16(pCtx, PWideChar(TCharacter.ToUpper(s)), -1, SQLITE_TRANSIENT);
+  s := SQLite3_Value_text16(Value);
+  SQLite3_Result_Text16(Context, PWideChar(TCharacter.ToUpper(s)), -1, TResultDestructor(SQLITE_TRANSIENT));
 end;
 
-procedure SystemLowerString(pCtx: TSQLite3Context; nArgs: Integer; Args: TSQLite3Value); cdecl;
+procedure SystemLowerString(Context: sqlite3_context; Num: Integer; out Value: sqlite3_value); cdecl;
 var
   s: string;
 begin
-  s := SQLite3_Value_text16(Args^);
-  SQLite3_Result_Text16(pCtx, PWideChar(TCharacter.ToLower(s)), -1, SQLITE_TRANSIENT);
+  s := SQLite3_Value_text16(Value);
+  SQLite3_Result_Text16(Context, PWideChar(TCharacter.ToLower(s)), -1, TResultDestructor(SQLITE_TRANSIENT));
 end;
 
 end.

Program/DAO/SQLite/Lib/SQLiteWrap.pas
@@ -26,7 +26,7 @@ uses
   {$IFDEF WIN32}
   Windows,
   {$ENDIF}
-  SQLite3,
+  System.Sqlite,
   Classes,
   SysUtils;
 
@@ -39,7 +39,7 @@ type
   {: @abstract(Class for handling SQLite database)}
   TSQLiteDatabase = class(TObject)
   private
-    FDB: TSQLite3DB;
+    FDB: sqlite3;
 
   protected
     procedure SetParams(query: TSQLiteQuery; const Params: array of const);
@@ -100,7 +100,7 @@ type
     { Add custom sorting procedure as new Collate.}
     procedure AddCustomCollate(
       const CollateName: string;
-      xCompare: TCollateXCompare;
+      xCompare: TxCompare;
       PrefferedEncoding: Integer = SQLITE_UTF16_ALIGNED
     );
 
@@ -113,14 +113,14 @@ type
     );
 
     { SQLite database handler.}
-    property DB: TSQLite3DB read FDB;
+    property DB: sqlite3 read FDB;
   end;
 
   {: @abstract(Class for handling SQLite query result)}
   TSQLiteQuery = class(TObject)
   private
     FDatabase: TSQLiteDatabase;
-    FStmt: TSQLiteStmt;
+    FStmt: sqlite3_stmt;
     FColCount: Longint;
     FCols: TStringList;
     FRow: Cardinal;
@@ -239,7 +239,8 @@ uses
   unit_Interfaces,
 {$ENDIF}
   Math,
-  SQLite3UDF;
+  SQLite3UDF,
+  unit_SQLiteUtils;
 
 const
   DATE_FORMAT = 'yyyy-mm-dd';
@@ -257,7 +258,7 @@ var
 begin
   inherited Create;
 
-  if SQLite3_Open(PUTF8Char(UTF8String(FileName)), FDB) <> SQLITE_OK then
+  if SQLite3_Open(PAnsiChar(UTF8String(FileName)), FDB) <> SQLITE_OK then
   begin
     if Assigned(FDB) then
       s := string(UTF8String(SQLite3_ErrMsg(FDB)))
@@ -452,7 +453,7 @@ end;
 
 function TSQLiteDatabase.LastInsertRowID: int64;
 begin
-  Result := SQLite3_LastInsertRowID(FDB);
+  Result := sqlite3_last_insert_rowid(FDB);
 end;
 
 function TSQLiteDatabase.LastChangedRows: int64;
@@ -462,19 +463,19 @@ end;
 
 procedure TSQLiteDatabase.SetTimeout(Value: Integer);
 begin
-  SQLite3_BusyTimeout(FDB, Value);
+  sqlite3_busy_timeout(FDB, Value);
 end;
 
 function TSQLiteDatabase.Version: string;
 begin
-  Result := string(UTF8String(SQLite3_Version));
+  Result := SQLITE_VERSION;
 end;
 
-procedure TSQLiteDatabase.AddCustomCollate(const CollateName: string; xCompare: TCollateXCompare; PrefferedEncoding: Integer = SQLITE_UTF16_ALIGNED);
+procedure TSQLiteDatabase.AddCustomCollate(const CollateName: string; xCompare: TXCompare; PrefferedEncoding: Integer = SQLITE_UTF16_ALIGNED);
 var
   nRc: Integer;
 begin
-  nRc := SQLite3_Create_Collation(FDB, PUTF8Char(UTF8String(CollateName)), PrefferedEncoding, nil, xCompare);
+  nRc := SQLite3_Create_Collation(FDB, PAnsiChar(UTF8String(CollateName)), PrefferedEncoding, nil, xCompare);
   if SQLITE_OK <> nRc then
     RaiseError('', 'CollateName');
 end;
@@ -483,7 +484,7 @@ procedure TSQLiteDatabase.AddFunction(const FunctionName: string; const nArg: In
 var
   nRc: Integer;
 begin
-  nRc := SQLite3_Create_Function(FDB, PUTF8Char(UTF8String(FunctionName)), nArg, PrefferedEncoding, pUserDate, @xFunc, nil, nil);
+  nRc := SQLite3_Create_Function(FDB, PAnsiChar(UTF8String(FunctionName)), nArg, PrefferedEncoding, pUserDate, @xFunc, nil, nil);
   if SQLITE_OK <> nRc then
     RaiseError('', FunctionName);
 end;
@@ -501,7 +502,7 @@ end;
 
 constructor TSQLiteQuery.Create(const DB: TSQLiteDatabase; const SQL: string);
 var
-  NextSQLStatement: PUTF8Char;
+  NextSQLStatement: PAnsiChar;
   i: Integer;
 begin
   inherited Create;
@@ -512,7 +513,7 @@ begin
   FColCount := 0;
   FSQL := SQL;
 
-  if SQLITE_OK <> Sqlite3_Prepare_v2(FDatabase.FDB, PUTF8Char(UTF8String(FSQL)), -1, FStmt, NextSQLStatement) then
+  if SQLITE_OK <> Sqlite3_Prepare_v2(FDatabase.FDB, PAnsiChar(UTF8String(FSQL)), -1, FStmt, NextSQLStatement) then
     DB.RaiseError(c_errorsql, FSQL);
 
   if not Assigned(FStmt) then
@@ -520,9 +521,9 @@ begin
 
   //get data types
   FCols := TStringList.Create;
-  FColCount := SQLite3_ColumnCount(FStmt);
+  FColCount := sqlite3_column_count(FStmt);
   for i := 0 to FColCount - 1 do
-    FCols.Add(string(UTF8String(SQLite3_ColumnName(FStmt, i))));
+    FCols.Add(string(UTF8String(sqlite3_column_name(FStmt, i))));
 end;
 
 destructor TSQLiteQuery.Destroy;
@@ -554,7 +555,7 @@ function TSQLiteQuery.GetParamIndex(const ParamName: string): Integer;
 var
   i: Integer;
 begin
-  i := SQLite3_bind_parameter_index(FStmt, PUTF8Char(UTF8String(ParamName)));
+  i := SQLite3_bind_parameter_index(FStmt, PAnsiChar(UTF8String(ParamName)));
   if i > 0 then
     Result := i - 1
   else
@@ -578,7 +579,7 @@ var
   valueUTF8: UTF8String;
 begin
   valueUTF8 := UTF8String(Value);
-  if SQLITE_OK <> SQLite3_bind_text(FStmt, ParamIndex + 1, PUTF8Char(valueUTF8), Length(valueUTF8), SQLITE_TRANSIENT) then
+  if SQLITE_OK <> SQLite3_bind_text(FStmt, ParamIndex + 1, PAnsiChar(valueUTF8), Length(valueUTF8), TBindDestructor(SQLITE_TRANSIENT)) then
     FDatabase.RaiseError(c_errorbindingparam, FSQL);
 end;
 
@@ -682,7 +683,7 @@ end;
 
 function TSQLiteQuery.GetFields(const I: Integer): string;
 begin
-  Result := string(UTF8String(Sqlite3_ColumnText(FStmt, i)));
+  Result := string(UTF8String(sqlite3_column_text(FStmt, i)));
 end;
 
 function TSQLiteQuery.GetFieldIndex(const FieldName: string): Integer;
@@ -703,7 +704,7 @@ end;
 
 function TSQLiteQuery.FieldIsNull(I: Integer): Boolean;
 begin
-  Result := SQLite3_ColumnText(FStmt, i) = nil;
+  Result := sqlite3_column_text(FStmt, i) = nil;
 end;
 
 function TSQLiteQuery.FieldAsString(I: Integer): string;
@@ -713,7 +714,7 @@ end;
 
 function TSQLiteQuery.FieldAsInt(I: Integer): Int64;
 begin
-  Result := SQLite3_ColumnInt64(FStmt, i);
+  Result := sqlite3_column_int64(FStmt, i);
 end;
 
 function TSQLiteQuery.FieldAsBoolean(I: Integer): Boolean;
@@ -734,7 +735,7 @@ end;
 
 function TSQLiteQuery.FieldAsDouble(I: Integer): Double;
 begin
-  Result := SQLite3_ColumnDouble(FStmt, i);
+  Result := sqlite3_column_double(FStmt, i);
 end;
 
 procedure TSQLiteQuery.LoadBlob(I: Integer; Stream: TStream);
@@ -743,10 +744,10 @@ var
   ptr: Pointer;
 begin
   Assert(Assigned(Stream));
-  iNumBytes := SQLite3_ColumnBytes(FStmt, i);
+  iNumBytes := sqlite3_column_bytes(FStmt, i);
   if iNumBytes > 0 then
   begin
-    ptr := Sqlite3_ColumnBlob(FStmt, i);
+    ptr := sqlite3_column_blob(FStmt, i);
     Stream.WriteBuffer(ptr^, iNumBytes);
     Stream.Position := 0;
   end;
@@ -760,8 +761,8 @@ end;
 
 function TSQLiteQuery.FieldAsBlobPtr(I: Integer; out iNumBytes: Integer): Pointer;
 begin
-  iNumBytes := SQLite3_ColumnBytes(FStmt, i);
-  Result := SQLite3_ColumnBlob(FStmt, i);
+  iNumBytes := sqlite3_column_bytes(FStmt, i);
+  Result := sqlite3_column_blob(FStmt, i);
 end;
 
 function TSQLiteQuery.FieldAsBlobString(I: Integer): string;

Program/DAO/SQLite/unit_SQLiteUtils.pas
@@ -24,13 +24,16 @@ uses
 
   function ReadResourceAsStringList(const ResourceName: string): TStringList;
 
+  function SQLiteErrorStr(SQLiteErrorCode: Integer): string;
+
 implementation
 
 uses
   Windows,
   unit_Consts,
   SysUtils,
-  StrUtils;
+  StrUtils,
+  System.Sqlite;
 
 const
   SQL_COMMENT = '--';
@@ -98,6 +101,43 @@ begin
   except
     FreeAndNil(Result);
     raise;
+  end;
+end;
+
+function SQLiteErrorStr(SQLiteErrorCode: Integer): String;
+begin
+  case SQLiteErrorCode of
+    SQLITE_OK: Result := 'Successful result';
+    SQLITE_ERROR: Result := 'SQL error or missing database';
+    SQLITE_INTERNAL: Result := 'An internal logic error in SQLite';
+    SQLITE_PERM: Result := 'Access permission denied';
+    SQLITE_ABORT: Result := 'Callback routine requested an abort';
+    SQLITE_BUSY: Result := 'The database file is locked';
+    SQLITE_LOCKED: Result := 'A table in the database is locked';
+    SQLITE_NOMEM: Result := 'A malloc() failed';
+    SQLITE_READONLY: Result := 'Attempt to write a readonly database';
+    SQLITE_INTERRUPT: Result := 'Operation terminated by sqlite3_interrupt()';
+    SQLITE_IOERR: Result := 'Some kind of disk I/O error occurred';
+    SQLITE_CORRUPT: Result := 'The database disk image is malformed';
+    SQLITE_NOTFOUND: Result := '(Internal Only) Table or record not found';
+    SQLITE_FULL: Result := 'Insertion failed because database is full';
+    SQLITE_CANTOPEN: Result := 'Unable to open the database file';
+    SQLITE_PROTOCOL: Result := 'Database lock protocol error';
+    SQLITE_EMPTY: Result := 'Database is empty';
+    SQLITE_SCHEMA: Result := 'The database schema changed';
+    SQLITE_TOOBIG: Result := 'Too much data for one row of a table';
+    SQLITE_CONSTRAINT: Result := 'Abort due to contraint violation';
+    SQLITE_MISMATCH: Result := 'Data type mismatch';
+    SQLITE_MISUSE: Result := 'Library used incorrectly';
+    SQLITE_NOLFS: Result := 'Uses OS features not supported on host';
+    SQLITE_AUTH: Result := 'Authorization denied';
+    SQLITE_FORMAT: Result := 'Auxiliary database format error';
+    SQLITE_RANGE: Result := '2nd parameter to sqlite3_bind out of range';
+    SQLITE_NOTADB: Result := 'File opened that is not a database file';
+    SQLITE_ROW: Result := 'sqlite3_step() has another row ready';
+    SQLITE_DONE: Result := 'sqlite3_step() has finished executing';
+  else
+    Result := 'Unknown SQLite Error Code "' + IntToStr(SQLiteErrorCode) + '"';
   end;
 end;
 

Program/DAO/SQLite/unit_SystemDatabase_SQLite.pas
@@ -204,7 +204,7 @@ uses
   Classes,
   Variants,
   IOUtils,
-  SQLite3,
+  System.Sqlite,
   unit_Logger,
   unit_SQLiteUtils,
   unit_Database_SQLite,

Program/MyhomeLib.dpr
@@ -118,7 +118,6 @@ uses
   frm_EditGroup in 'Forms\Editors\frm_EditGroup.pas' {frmEditGroup},
   unit_Logger in 'Units\unit_Logger.pas',
   unit_Interfaces in 'Units\unit_Interfaces.pas',
-  SQLite3 in 'DAO\SQLite\Lib\SQLite3.pas',
   SQLite3UDF in 'DAO\SQLite\Lib\SQLite3UDF.pas',
   SQLiteWrap in 'DAO\SQLite\Lib\SQLiteWrap.pas',
   unit_Database_SQLite in 'DAO\SQLite\unit_Database_SQLite.pas',

Program/MyhomeLib.dproj
@@ -272,7 +272,6 @@ $(PostBuildEvent)]]></PostBuildEvent>
         </DCCReference>
         <DCCReference Include="Units\unit_Logger.pas"/>
         <DCCReference Include="Units\unit_Interfaces.pas"/>
-        <DCCReference Include="DAO\SQLite\Lib\SQLite3.pas"/>
         <DCCReference Include="DAO\SQLite\Lib\SQLite3UDF.pas"/>
         <DCCReference Include="DAO\SQLite\Lib\SQLiteWrap.pas"/>
         <DCCReference Include="DAO\SQLite\unit_Database_SQLite.pas"/>
@@ -446,7 +445,7 @@ $(PostBuildEvent)]]></PostBuildEvent>
                         <Overwrite>true</Overwrite>
                     </Platform>
                 </DeployFile>
-                <DeployFile LocalName="Out\Bin\MyHomeLib.exe" Configuration="Release" Class="ProjectOutput">
+                <DeployFile LocalName="Win32\Release\MyHomeLib.exe" Configuration="Release" Class="ProjectOutput">
                     <Platform Name="Win32">
                         <RemoteName>MyhomeLib.exe</RemoteName>
                         <Overwrite>true</Overwrite>
@@ -862,13 +861,13 @@ $(PostBuildEvent)]]></PostBuildEvent>
                         <Operation>1</Operation>
                     </Platform>
                 </DeployClass>
-                <ProjectRoot Platform="iOSDevice32" Name="$(PROJECTNAME).app"/>
+                <ProjectRoot Platform="iOSSimulator" Name="$(PROJECTNAME).app"/>
                 <ProjectRoot Platform="Android" Name="$(PROJECTNAME)"/>
                 <ProjectRoot Platform="Win32" Name="$(PROJECTNAME)"/>
                 <ProjectRoot Platform="iOSDevice64" Name="$(PROJECTNAME).app"/>
                 <ProjectRoot Platform="OSX32" Name="$(PROJECTNAME).app"/>
                 <ProjectRoot Platform="Win64" Name="$(PROJECTNAME)"/>
-                <ProjectRoot Platform="iOSSimulator" Name="$(PROJECTNAME).app"/>
+                <ProjectRoot Platform="iOSDevice32" Name="$(PROJECTNAME).app"/>
             </Deployment>
         </BorlandProject>
         <ProjectFileVersion>12</ProjectFileVersion>

commit 742492474125d2ea1d4acace30a8b983cd98daa9

Fix bug introduced in 0685882b0

Program/DAO/SQLite/unit_Database_SQLite.pas
@@ -1034,16 +1034,17 @@ end;
 
 { TBookCollection_SQLite }
 function CreateFullAuthorName(Context: sqlite3_context; Num: Integer; out Value: sqlite3_value): string; inline;
+type
+  sqlite3_value_array = array[0..MaxShort - 1] of sqlite3_value;
+  sqlite3_value_array_ptr = ^sqlite3_value_array;
 var
   LastName: string;
   FirstName: string;
   MiddleName: string;
 begin
-  LastName := SQLite3_Value_text16(Value);
-  Inc(NativeInt(Value));
-  FirstName := SQLite3_Value_text16(Value);
-  Inc(NativeInt(Value));
-  MiddleName := SQLite3_Value_text16(Value);
+  LastName := SQLite3_Value_text16(sqlite3_value_array_ptr(@Value)[0]);
+  FirstName := SQLite3_Value_text16(sqlite3_value_array_ptr(@Value)[1]);
+  MiddleName := SQLite3_Value_text16(sqlite3_value_array_ptr(@Value)[2]);
 
   Result := TAuthorData.FormatName(LastName, FirstName, MiddleName);
 end;